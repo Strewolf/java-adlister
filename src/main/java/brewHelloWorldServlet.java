215c7190434760a25748910e79d35c54d1f6b6b3
Horace Smith <horacesmith26@gmail.com>
2023-03-09 11:36:22 -0600

    servlet warm up

--- src/main/java/brewHelloWorldServlet.java
+++ src/main/java/brewHelloWorldServlet.java
@@ -5,14 +5,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-
-@WebServlet("/hello")
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+@WebServlet("/zenith")
 public class brewHelloWorldServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-//        System.out.println("hello world");
-        PrintWriter out = res.getWriter();
-        out.println("<h1>hello world<h1><br><h3>this is a smaller heading<h3>");
+    private int counter = 0;
+@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+//        PrintWriter hw = response.getWriter();
+        out.println("<html><head><title>Page View Counter</title></head><body>");
 
+        String name = request.getParameter("name");
+        if ( name == null){
+            out.println("<h1>Hello World</h1>");
+        } else {
+            out.println("hello " + name + "!");
+        }
+
+        // Bonus: Check if reset parameter exists in query string
+        String reset = request.getParameter("reset");
+        if (reset != null && reset.equals("true")) {
+            counter = 0;
+            out.println("<h1>Counter has been reset</h1>");
+        } else {
+            counter++;
+            out.println("<h1>Page Views: " + counter + "</h1>");
+        }
+
+        out.println("</body></html>");
     }
+
 }
