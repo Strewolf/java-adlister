35a0a01d44cfdb2cca6eb43e81773d7d53152974
Horace Smith <horacesmith26@gmail.com>
2023-03-14 09:38:53 -0500

    BeanTest

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,13 +1,53 @@
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//@WebServlet(name = "LoginServlet", urlPatterns = "/login")
+//public class LoginServlet extends HttpServlet {
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("/login.jsp").forward(request, response);
+//        HttpSession session = request.getSession(false);
+//        if (session != null && session.getAttribute("user") != null) {
+//            response.sendRedirect("/WEB-INF/profile");
+//            return;
+//        }
+//
+//    }
+//
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//        boolean validAttempt = username.equals("admin") && password.equals("password");
+//
+//        if (validAttempt) {
+//            response.sendRedirect("/WEB-INF/profile");
+//        } else {
+//            response.sendRedirect("/login");
+//        }
+//
+//    }
+//
+//}
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null && session.getAttribute("user") != null) {
+            response.sendRedirect("/profile");
+            return;
+        }
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
@@ -17,9 +57,13 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
+
     }
+
 }
--- src/main/webapp/partials/navbar.jsp
+++ src/main/webapp/partials/navbar.jsp
@@ -4,9 +4,22 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="/ads">Adlister</a>
         </div>
+        <% if (session != null && session.getAttribute("user") != null) { %>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="/login">Login</a></li>
+            <li class="nav-item">
+                <a class="nav-link" href="/profile">Welcome <%= session.getAttribute("user") %></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/logout">Logout</a>
+            </li>
         </ul>
+        <% } else { %>
+        <ul class="nav navbar-nav navbar-right">
+            <li class="nav-item">
+                <a class="nav-link" href="/login">Login</a>
+            </li>
+        </ul>
+        <% } %>
     </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
 </nav>
